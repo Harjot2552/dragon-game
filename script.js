9d23cf271cbd1bffcdc836e0cb9f61fce6b28308
Harjot <hajotsingh2552@gmail.com>
2025-02-03 11:55:23 -0600

    Write the logic for the up left and right arrow key, so whenever these keys are down their will be animation which will be remove and addded to the dragon and obstacle

--- script.js
+++ script.js
@@ -7,3 +7,24 @@ audiogo = new Audio('gameover.mp3')
 setTimeout(() => {
     bgmusic.play();
 }, 1000);
+
+
+document.onkeydown = function (e) {
+    if (e.keyCode == 38) {
+        dino = document.querySelector(".dino")
+        dino.classList.add('animateDino')
+        setTimeout(() => {
+            dino.classList.remove('animateDino')
+        }, 700)
+    }
+    if (e.keyCode == 39) {
+        dino = document.querySelector(".dino")
+        dinoX = parseInt(window.getComputedStyle(dino, null).getPropertyValue('left'));
+        dino.style.left = dinoX + 112 + "px";
+    }
+    if (e.keyCode == 37) {
+        dino = document.querySelector(".dino")
+        dinoX = parseInt(window.getComputedStyle(dino, null).getPropertyValue('left'));
+        dino.style.left = (dinoX - 112) + "px";
+    }
+}
